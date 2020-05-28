cfda7178a9a6468e3bbfd8c489ba5827fcbe5e1a
Emanoel580 <61793443+Emanoel580@users.noreply.github.com>
2020-05-28 20:55:42 -0300

    Delete fabio03_for_10.py

--- fabio03_for_10.py
+++
@@ -1,9 +0,0 @@
-def main():
-    limite_inferior = int(input('inferior: '))
-    limite_superior = int(input('superior: '))
-    for c in range(limite_inferior, limite_superior):
-        if c % 2 != 0:
-            print(c, end=' ')
-
-
-main()
